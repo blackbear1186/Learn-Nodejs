3bf2d1e879331eda46e98946a70218f7ac2a7c9c
Berlin Johnson <blackbear@Berlins-MacBook-Pro.local>
2021-02-09 00:27:46 +0900

    Work with nodejs modules

--- fibonacci.js
+++ fibonacci.js
@@ -1,19 +1,19 @@
 // 1 1 2 3 5 8 13 21
-'use strict';
-// use const args inorder to use command line arguments
-const args = process.argv.slice(2);
+"use strict";
 
-const generateFibonacci = howMany => {
-  let x = 0,
-      y = 1;
-  let sum;
-  let i = 0;
-  for(i = 0; i < howMany; i++){
-    sum = x + y;
-    x = y;
-    y = sum;
-  }
-  return y;
-}
-const howMany = args[0];
-console.log(generateFibonacci(howMany));
+export const sayHello = () => console.log('What Up');
+
+export const generateFibonacci = (howMany) => {
+  let x = 1,
+    y = 1;
+  let sequence = [x, y];
+
+  Array(howMany)
+    .fill()
+    .forEach(() => {
+      let sum = x + y;
+      x = y;
+      y = sum;
+    });
+  return sequence;
+};
---
+++ index.js
@@ -0,0 +1,10 @@
+import {sayHello, generateFibonacci} from './fibonacci.js';
+
+sayHello();
+console.log(generateFibonacci(10))
+
+// import all functions from file
+// import * as all from './fibonacci.js';
+
+// all.sayHello();
+// console.log(all.generateFibonacci(10))
